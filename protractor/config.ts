21b93564867a04e7ab0642d09b91dc69fa35284f
Holmes Salazar <hgsosorio@gmail.com>
2017-10-09 10:12:03 -0500

    PSL-27: Using async/await in tests instead of promise manager

--- protractor/config.ts
+++ protractor/config.ts
@@ -2,8 +2,8 @@ import { browser, Config } from 'protractor';
 import { reporter }   from './helpers/reporter';
 
 export const config: Config = {
+  SELENIUM_PROMISE_MANAGER: false,
   framework: 'jasmine',
-  seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['../test/Google.spec.js'],
   noGlobals: true,
   onPrepare: () => {
