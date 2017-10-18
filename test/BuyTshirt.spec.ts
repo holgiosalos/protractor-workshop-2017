5d475c819aa633fd050bdfdbe39a7ef76cdf5fba
Holmes Salzar <hgsosorio@gmail.com>
2017-10-18 16:19:03 -0500

    Using several describes in a BDD way

--- test/BuyTshirt.spec.ts
+++ test/BuyTshirt.spec.ts
@@ -13,35 +13,56 @@ import {
   OrderResumePage
 } from '../src/page';
 
-describe('Buy a t-shirt', () => {
-  const menuContentPage: MenuContentPage = new MenuContentPage();
-  const productList: ProductListPage = new ProductListPage();
-  const productDetail: ProductDetailPage = new ProductDetailPage();
-  const productAddedModal: ProductAddedModalPage = new ProductAddedModalPage();
-  const summaryStep: SummaryStepPage = new SummaryStepPage();
-  const signInStep: SignInStepPage = new SignInStepPage();
-  const addressStep: AddressStepPage = new AddressStepPage();
-  const shippingStep: ShippingStepPage = new ShippingStepPage();
-  const paymentStep: PaymentStepPage = new PaymentStepPage();
-  const bankPayment: BankPaymentPage = new BankPaymentPage();
-  const orderResume: OrderResumePage = new OrderResumePage();
-
-  it('then should be bought a t-shirt', async () => {
+describe('Given I open a shopping page', () => {
+  beforeAll(async() => {
     await browser.get('http://automationpractice.com/');
+  });
 
-    await menuContentPage.goToTShirtMenu();
-    await productList.openFirstProductDetail();
-    await productDetail.addToCart();
-    await productAddedModal.proceedToCheckout();
-    await summaryStep.proceedToCheckout();
+  describe('When I want to buy a t-shirt', () => {
+    beforeAll(async() => {
+      const menuContentPage: MenuContentPage = new MenuContentPage();
+      const productList: ProductListPage = new ProductListPage();
+      const productDetail: ProductDetailPage = new ProductDetailPage();
+      const productAddedModal: ProductAddedModalPage = new ProductAddedModalPage();
+      const summaryStep: SummaryStepPage = new SummaryStepPage();
 
-    await signInStep.login('aperdomobo@gmail.com', 'WorkshopProtractor');
+      await menuContentPage.goToTShirtMenu();
+      await productList.openFirstProductDetail();
+      await productDetail.addToCart();
+      await productAddedModal.proceedToCheckout();
+      await summaryStep.proceedToCheckout();
+    });
 
-    await addressStep.proceedToCheckout();
-    await shippingStep.agreeTOSAndProceedToCheckout();
-    await paymentStep.payByBankWire();
-    await bankPayment.confirmOrder();
+    describe('And I log in into the shopping page', () => {
+      beforeAll(async() => {
+        const signInStep: SignInStepPage = new SignInStepPage();
+        await signInStep.login('aperdomobo@gmail.com', 'WorkshopProtractor');
+      });
 
-    await expect(orderResume.getPayoutResultTitle()).toBe('Your order on My Store is complete.');
+      describe('And I select the address by default', () => {
+        beforeAll(async() => {
+          const addressStep: AddressStepPage = new AddressStepPage();
+          const shippingStep: ShippingStepPage = new ShippingStepPage();
+
+          await addressStep.proceedToCheckout();
+          await shippingStep.agreeTOSAndProceedToCheckout();
+        });
+
+        describe('And I select the bank wire pay option', async() => {
+          beforeAll(async() => {
+            const paymentStep: PaymentStepPage = new PaymentStepPage();
+            const bankPayment: BankPaymentPage = new BankPaymentPage();
+
+            await paymentStep.payByBankWire();
+            await bankPayment.confirmOrder();
+          });
+
+          it('then should be bought a t-shirt', async () => {
+            const orderResume: OrderResumePage = new OrderResumePage();
+            await expect(orderResume.getPayoutResultTitle()).toBe('Your order on My Store is complete.');
+          });
+        });
+      });
+    });
   });
 });
